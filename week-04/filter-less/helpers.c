e212c5d65b312691ca6d306da8db59045928e8b8
dustein <steindu@gmail.com>
2024-06-11 09:05:01 -0300

    retomando filter-less

--- week-04/filter-less/helpers.c
+++ week-04/filter-less/helpers.c
@@ -6,15 +6,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     int pixel_red;
     int pixel_green;
     int pixel_blue;
-
+    int new_color;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int actual_pixel = RGBTRIPLE image[i][j];
-            pixel_red = actual_pixel.rgbtRed;
-            pixel_gree = actual_pixel.rgbtGreen;
-            pixel_blue = actual_pixel.rgbtBlue;
+            pixel_red = image[i][j].rgbtRed;
+            pixel_green = image[i][j].rgbtGreen;
+            pixel_blue = image[i][j].rgbtBlue;
+            new_color = round((pixel_red + pixel_green + pixel_blue) / 3.0);
+            pixel_red = new_color;
+            pixel_green = new_color;
+            pixel_blue = new_color;
         }
     }
     return;

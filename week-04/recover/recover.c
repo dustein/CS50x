1e3e4ed8edf648cceda396d852f80a71218fca72
dustein <steindu@gmail.com>
2024-06-20 15:16:11 -0300

    continue recovery jpg

--- week-04/recover/recover.c
+++ week-04/recover/recover.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,17 +17,26 @@ int main(int argc, char *argv[])
     FILE *card = fopen(argv[1], "r");
     if (card == NULL)
     {
-        printf("Couldn`t open %s", card);
+        //printf("Couldn`t open %s", card);
+        printf("Could Not Open Card");
         return 0;
     }
     // While there's still data left to read from the memory card
-    uint8_t buffer[BLOCK_SIZE];
-    while (fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE)
+    uint8_t buffer[512];
+    int file_num = 0;
+    while (fread(&buffer, sizeof(BLOCK_SIZE), 1, card) == BLOCK_SIZE)
     {
-        // Create JPEGs from the data
+        //if tiver esses bytes no bloco de 512KB, é o início de um jpg
         if (buffer[0] == 0xff & buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
+            // Create JPEGs from the data
+            FILE* imagem = fopen(imagem, "w");
+            fprintf(imagem, "%03i.jpg", file_num);
+            fwrite(&buffer, sizeof(BLOCK_SIZE), 1, imagem);
+            fclose(imagem);
+            file_num++;
 
         }
     }
+    fclose(card);
 }

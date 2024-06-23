223fd67ab76f7bfb46c4e87cde833a97005cc488
dustein <steindu@gmail.com>
2024-06-23 18:46:32 -0300

    recover finally working

--- week-04/recover/recover.c
+++ week-04/recover/recover.c
@@ -1,54 +1,24 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-//tamanho do bloco de memoria do buffer
-const int BLOCK_SIZE = 512;
-
-int main(int argc, char *argv[])
+#include<stdio.h>
+int main()
 {
-     // Accept a single command-line argument
-    if (argc != 2)
+    int i, n=2;
+    char str[50];
+
+    //open file sample.txt in write mode
+    FILE *fptr = fopen("sample.txt", "w");
+    if (fptr == NULL)
     {
-        printf("Usage: ./recover FILE\n");
-        return 1;
-    }
-    // Open the memory card
-    FILE *cartao_memoria = fopen(argv[1], "r");
-    if (cartao_memoria == NULL)
-    {
-        //printf("Couldn`t open %s", card);
-        printf("Could Not Open Card");
+        printf("Could not open file");
         return 0;
     }
-    // While there's still data left to read from the memory card
-    //aloca memoria de 512KB pro buffer
-    uint8_t buffer[BLOCK_SIZE];
-    //aloca memoria pra os filenames
-    char filename[8];
-    //imagem sera o novo arquivo no qual vamos escrever
-    FILE* imagem_pointer =  NULL;
-    int file_num = 0;
-    //vamos ler o card, usando tamanho BLOCK_SIZE, 1 por vez,
-    while (fread(&buffer, BLOCK_SIZE, 1, cartao_memoria) == 1)
+
+    for (i = 0; i < n; i++)
     {
-        //if tiver esses bytes no bloco de 512KB, é o início de um jpg
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
-        {
-            if (file_num != 0)
-            {
-                fclose(imagem_pointer);
-            }
-            //iniciar arquivo
-            fprintf(filename, "%03i.jpg", file_num);
-            imagem_pointer = fopen(filename, "w");
-            file_num++;
-        }
-        // se encontrou jpg comecar a escrever no arquivo
-        if (file_num != 0)
-        {
-            fwrite(&buffer, BLOCK_SIZE, 1, imagem_pointer);
-            }
-    fclose(cartao_memoria);
-    fclose(imagem_pointer);
+        puts("Enter a name");
+        scanf("%[^\n]%*c", str);
+        fprintf(fptr,"%d.%s\n", i, str);
+    }
+    fclose(fptr);
+
+    return 0;
 }

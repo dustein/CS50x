70f1b7d1801883e716f219ba85c03d6950c1c17b
dustein <steindu@gmail.com>
2024-07-01 09:47:13 -0300

    speller completed

--- week-05/speller/dictionary.c
+++ week-05/speller/dictionary.c
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -17,6 +17,7 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
+// aqui sao os buckets para cada letra inicial. Podemos melhorar criando mais buckets
 const unsigned int N = 26;
 int conta_palavra = 0;
 
@@ -33,16 +34,16 @@ bool check(const char *word)
     // iniciamos apontando o cursos para o head, que é a table na posicao indice_hash
     node *cursor = table[indice_hash];
     // vamos percorrer a lista ate o final, ou seja, ate chegar no NULL que é o seu fim
-    while (cursor != NULL)
+    while (cursor != 0)
     {
         // checar se a palavra e a mesma encotrada pelo cursos
-        if(strcasecmp(cursor -> word, word) == 0)
+        if (strcasecmp(word, cursor->word) == 0)
         {
             // se a funcao retornar 0 é porque encontrou a palavra
             return true;
         }
         // se nao for 0, nao encontrou a palavra, passar o cursos para a proxima
-        cursor = cursor -> next;
+        cursor = cursor->next;
     }
     return false;
 }
@@ -51,52 +52,48 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // A funcao deve receber uma atring e retornar um indice
+    // return toupper(word[0]) - 'A';
+    unsigned long soma = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        soma += tolower(word[i]);
+    }
+    return soma % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    //abrir o arquivo dicionario
+    // abrir o arquivo dicionario
     FILE *dicionario = fopen(dictionary, "r");
     if (dicionario == NULL)
     {
+        printf("Nao foi possivel abrir %s\n", dictionary);
         return false;
     }
     // ler as strings que tem no arquivo.
-    //fscanf vai teronar EOF quando não houver mais string para ler
-    char palavra[LENGTH + 1]; // + 1 porque no final de toda string temos o operador NUL
-    while(fscanf(dicionario, "%s", palavra) != EOF)
+    // fscanf vai teronar EOF quando não houver mais string para ler
+    char word[LENGTH + 1]; // + 1 porque no final de toda string temos o operador NUL
+
+    while (fscanf(dicionario, "%s", word) != EOF)
     {
-        // para fazer uma linked list, temos que criar um node, cuja estrutura é composta por uma palavra e um ponteiro para o próximo node
-        // vamos alocar memoria usando a variavel tipo node criada na strut no inicio do codigo
-        node *node_alocado = malloc(sizeof(node));
-        if (node_alocado == NULL)
+        // para fazer uma linked list, temos que criar um node, cuja estrutura é composta por uma
+        // palavra e um ponteiro para o próximo node vamos alocar memoria usando a variavel tipo
+        // node criada na strut no inicio do codigo
+        node *no = malloc(sizeof(node));
+        if (no == NULL)
         {
             return false;
         }
         // agora usando strcopy vamos copiar a palavra para o node (word da estruct node)
-        strcpy(node_alocado -> word, palavra);
+        strcpy(no->word, word);
         // implementar a função hash trazida no código, que depois vamos melhorar
         // a funcao hash recebe uma string e retorna um indice
-        int indice = hash(palavra);
-        // fazendo a lista,
-        // checar se o header item aponta para NULL, se existe algum item
-        if (table[indice] == NULL)
-        {
-            // nesse caso, vamos inserir o primeiro item, primeiro apontamos o indice para NULL, usando o next definido na estruct
-            node_alocado -> next = NULL;
-        }
-        else
-        {
-            // ou apontar para o primeiro node da lista
-            node_alocado -> next = table[indice];
-        }
-        // apontar o header para o indice
-        table[indice] = node_alocado;
-
-        // vamos contar quantas palavras carregamos conforme lemor o dicionario
+        int indice_hash = hash(word);
+        no->next = table[indice_hash];
+        table[indice_hash] = no;
         conta_palavra++;
     }
 
@@ -108,30 +105,25 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    // aqui para retornar o tamanho do dicionario é só usarmos o conta_palavras que usamos na funcao load
+    // aqui para retornar o tamanho do dicionario é só usarmos o conta_palavras que usamos na funcao
+    // load
     return conta_palavra;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
-// funcao auxiliar para liberar memoria dos node no final
-void free_node(node *n)
-{
-    if (n -> next != NULL)
-    {
-        free_node(n -> next);
-    }
-    free(n);
-}
 
 bool unload(void)
 {
     // TODO
     for (int i = 0; i < N; i++)
     {
-        if (table[i] != NULL)
+        node *cursor = table[i];
+        while (cursor != NULL)
         {
-            free_node(table[i]);
+            node *temporario = cursor;
+            cursor = cursor->next;
+            free(temporario);
         }
     }
-    return false;
+    return true;
 }

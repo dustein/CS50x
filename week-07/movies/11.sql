1f7f6a7acc97e124939203e1feb3adecbb55ddf5
dustein <steindu@gmail.com>
2024-08-03 20:35:38 -0300

    starting fiftyville

--- week-07/movies/11.sql
+++ week-07/movies/11.sql
@@ -1,32 +1,18 @@
+SELECT title
+FROM movies JOIN ratings
+ON movies.id = ratings.movie_id
+WHERE id IN (
+    SELECT movie_id
+    FROM stars
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Chadwick Boseman'
+    )
+)
+ORDER BY rating DESC
+LIMIT 5;
 
-
--- CREATE TABLE directors (
---     movie_id INTEGER NOT NULL,
---     person_id INTEGER NOT NULL,
---     FOREIGN KEY(movie_id) REFERENCES movies(id),
---     FOREIGN KEY(person_id) REFERENCES people(id)
--- );
--- CREATE TABLE movies (
---     id INTEGER,
---     title TEXT NOT NULL,
---     year NUMERIC,
---     PRIMARY KEY(id)
--- );
--- CREATE TABLE people (
---     id INTEGER,
---     name TEXT NOT NULL,
---     birth NUMERIC,
---     PRIMARY KEY(id)
--- );
--- CREATE TABLE ratings (
---     movie_id INTEGER NOT NULL,
---     rating REAL NOT NULL,
---     votes INTEGER NOT NULL,
---     FOREIGN KEY(movie_id) REFERENCES movies(id)
--- );
--- CREATE TABLE stars (
---     movie_id INTEGER NOT NULL,
---     person_id INTEGER NOT NULL,
---     FOREIGN KEY(movie_id) REFERENCES movies(id),
---     FOREIGN KEY(person_id) REFERENCES people(id)
-
+-- SELECT id
+-- FROM people
+-- WHERE name = 'Chadwick Boseman';

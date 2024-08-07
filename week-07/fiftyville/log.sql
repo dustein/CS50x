ffdfa57631615d960fc0d7e168f88a5edc598347
dustein <steindu@gmail.com>
2024-08-07 09:44:24 -0300

    filtrando resultados

--- week-07/fiftyville/log.sql
+++ week-07/fiftyville/log.sql
@@ -1,2 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Who the thief is,
+-- What city the thief escaped to, and
+-- Who the thief’s accomplice is who helped them escape
+

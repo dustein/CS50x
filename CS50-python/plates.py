de05d3a3acc537daee6ecdd006869dd751852829
dustein <steindu@gmail.com>
2025-01-06 19:20:47 -0300

    plates fixed

--- CS50-python/plates.py
+++ CS50-python/plates.py
@@ -24,16 +24,20 @@ def is_valid(s):
 
 # testar se a partr do primeior numero so haja numero e o prmero nao e zero
 def final_num(word):
-    for i in range(len(word)):
-        # print(i, "-", word[i])
-        if word[i].isnumeric():
-            # letras = word[0:i]
-            numeros = word[i:]
-            
-            for numero in numeros:
-                if numero == "0" or numero.isalpha():
-                    return True
-                    # print("zero ou letra msturada")
+    # for i in range(len(word)):
+    #     if word[i].isnumeric():
+    #         numeros = word[i:]
+    #         for numero in numeros:
+    #             if numero == "0" or numero.isalpha():
+    #                 return True
+    found_a_number = False
+    for letter in word:
+        if letter.isnumeric():
+            if not found_a_number and letter == "0":
+                return True
+            found_a_number = True
+        elif found_a_number and letter.isalpha():
+            return True
 
 def check_alfanum(word):
     for letter in word:

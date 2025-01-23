d357ab1b75f8ffd3a5d49217cd3a90b761f7f015
dustein <steindu@gmail.com>
2025-01-23 16:58:10 -0300

    week 5 start

--- CS50-python/bitcoin.py
+++ CS50-python/bitcoin.py
@@ -1,16 +1,28 @@
+# pip install requests
 import sys
-
+import requests
 
 def main():
 
-    if 1 > argv > 2:
-        print("falta argv ou tem demais")
+
     try:
-        coins = float(argv[1])
+        if len(sys.argv) != 2:
+            print("Missing command-line argument")
+            sys.exit(1)
+        coins = float(sys.argv[1])
+
     except ValueError:
         print("Comand-line argument is not a number")
-        pass
+        sys.exit(1)
 
+    try:
+        dados = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+        rate = dados["bpi"]["USD"]["rate_float"]
+        resposta = coins * rate
+
+        print(f"${resposta:,.4f}")
+    except requests.RequestException:
+        pass
 
 if __name__ == "__main__":
     main()

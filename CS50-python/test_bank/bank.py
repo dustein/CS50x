ac93e0cb187de3503c3cebf106476d1a78ecaa15
dustein <steindu@gmail.com>
2025-01-24 16:31:31 -0300

    test bank corrected

--- CS50-python/test_bank/bank.py
+++ CS50-python/test_bank/bank.py
@@ -1,16 +1,15 @@
 def main():
     frase = input("Greeting: ").lower().strip()
-    print(value(frase))
-    return
+    print(f"${value(frase)}")
 
 def value(greeting):
 
     if (greeting.startswith("hello ") or greeting.startswith("hello")):
-        return "$0"
+        return 0
     elif greeting.startswith("h"):
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()

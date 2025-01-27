201f16fe045e28aacd6b0e7127be0bf93944820e
dustein <steindu@gmail.com>
2025-01-27 11:36:37 -0300

    pizza done

--- CS50-python/pizza/pizza.py
+++ CS50-python/pizza/pizza.py
@@ -5,27 +5,28 @@ from tabulate import tabulate
 def main():
     if len(sys.argv) < 2:
         print("Too few command-line arguments")
+        sys.exit(1)
     elif len(sys.argv) > 2:
         print("Too many command-line arguments")
+        sys.exit(1)
     elif not sys.argv[1].endswith(".csv"):
         print("not a CSV file")
+        sys.exit(1)
 
     menu = []
 
     try:
         with open(sys.argv[1]) as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.reader(csvfile)
 
-            for name, small, large in reader:
-                menu.append({"name": name, "small": small, "large": large})
-                # print(tabulate({row["Sicilian Pizza"], row["Large"], row["Small"]}, headers="firstrow"))
-
-            for pizza in menu:
-                print(f"{pizza['name']}")
+            for row in reader:
+                menu.append(row)
 
     except(FileNotFoundError):
         print("filenotfou error")
         sys.exit(1)
+
+    print(tabulate(menu, headers='firstrow', tablefmt="grid"))
 
 if __name__ == "__main__":
     main()

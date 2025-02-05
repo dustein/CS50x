497b4518843bf2e942870417a8123511d298ae11
dustein <steindu@gmail.com>
2025-02-05 16:19:59 -0300

    finishing shirt

--- CS50-python/shirt/shirt.py
+++ CS50-python/shirt/shirt.py
@@ -4,39 +4,36 @@ from PIL import Image, ImageOps
 
 def main():
     if len(sys.argv) < 3:
-        print("Too few command-line arguments")
-        sys.exit(1)
+        # print("Too few command-line arguments")
+        sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
-        print("Too many command-line arguments")
-        sys.exit(1)
+        # print("Too many command-line arguments")
+        sys.exit("Too many command-line arguments")
 
     img_in = sys.argv[1].lower()
     img_out = sys.argv[2].lower()
     if not img_in.endswith((".jpg", ".jpeg", ".png")) or not img_out.endswith((".jpg", ".jpeg", ".png")):
-        print("Invalid input")
-        sys.exit(1)
+        # print("Invalid input")
+        sys.exit("Invalid input")
     elif not os.path.splitext(img_in)[1] == os.path.splitext(img_out)[1]:
         print("Input and output have different extensions")
-        sys.exit(1)
+        sys.exit("Input and output have different extensions")
 
     try:
-        with Image.open(img_in) as image_in, Image.open("shirt.png") as shirt:
+        with Image.open(img_in) as image_in, Image.open("./shirt.png") as shirt:
             # pega o tamanho da imagem
-            get_size = image_in.size
+            get_size = shirt.size
             print(get_size)
             # redimensiona a shirt pro tamanho da imagem
-            ImageOps.fit(shirt, get_size)
+            imagem = ImageOps.fit(image=image_in, size=get_size)
             # sobrepoe a shirt na imagem
-            image_in.paste(img_in, (0, 0), shirt)
-            shirt.save(img_out)
-
-
-
+            imagem.paste(im=shirt, mask=shirt)
+            imagem.save(img_out)
 
 
     except(FileNotFoundError):
-        print("File not found")
-        sys.exit(1)
+        # print("File not found")
+        sys.exit("File not found")
 
 if __name__ == "__main__":
     main()

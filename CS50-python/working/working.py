acb72c6936481dac5ce43ffd527c14a44bec84a9
dustein <steindu@gmail.com>
2025-06-10 15:35:34 -0300

    continuando

--- CS50-python/working/working.py
+++ CS50-python/working/working.py
@@ -1,71 +1,103 @@
+# import re
+# import sys
+
+
+# def main():
+#     print(convert(input("Hours: ")))
+
+
+# def convert(s):
+#     padrao = r"^((0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))\s?to\s?((0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))$"
+#     try:
+#         timeGiven = re.search(padrao, s)
+#         if timeGiven == None:
+#             raise ValueError
+
+#     except ValueError:
+#         return 1
+
+#     print("2:") # hora inicial
+#     print(timeGiven.group(2))
+#     hora_inicial = timeGiven.group(2)
+
+#     print("3:") # minutos iniciais
+#     print(timeGiven.group(3))
+#     minutos_inicial = timeGiven.group(3)
+#     if minutos_inicial == None:
+#         minutos_inicial = 00
+
+#     print("4:") # AM/PM
+#     print(timeGiven.group(4))
+#     am_pm_inicial = timeGiven.group(4).upper()
+
+#     print("6:") # hora final
+#     print(timeGiven.group(6))
+#     hora_final = timeGiven.group(6)
+
+#     print("7:") # minutos final
+#     print(timeGiven.group(7))
+#     minutos_final = timeGiven.group(7)
+#     if minutos_final == None:
+#         minutos_final = 00
+
+#     print("8:") # AM/PM
+#     print(timeGiven.group(8))
+#     am_pm_final = timeGiven.group(8).upper()
+
+#     if am_pm_final == "PM" and hora_final != 12:
+#         hora_final += 12
+#     elif am_pm_final == "AM" and hora_final == 12:
+#         hora_final = 0
+
+#     return f'{hora_final}:{minutos_final}'
+
+
+# ...
+# # 06:00 AM to 09:30 PM
+# # 6:10 AM to 9:00 PM
+
+# if __name__ == "__main__":
+#     main()
+
 import re
 import sys
 
-
 def main():
-    print(convert(input("Hours: ")))
+
+    try:
+        time_input = input("Hours: ")
+        print(convert(time_input))
+
+    except ValueError:
+        sys.exit("ValueError")
 
 
 def convert(s):
-    # padrao = r"^(?:([0-9]|1[0-2])(?::[0-5][0-9])?\s?(AM|PM))\s?to\s?(?:([0-9]|1[0-2])(?::[0-5][0-9])?\s?(AM|PM))$"
-    # padrao = r"^((?:[0-9]|1[0-2])(?::[0-5][0-9])?\s?(?:AM|PM))\s?to\s?((?:[0-9]|1[0-2])(?::[0-5][0-9])?\s?(?:AM|PM))$"
-    # padrao = r"^(([0-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))\s?to\s?(([0-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))$"
-    padrao = r"^((0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))\s?to\s?((0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s?([AP]M))$"
-    try:
-        timeGiven = re.search(padrao, s)
-        if timeGiven == None:
-            raise ValueError
 
-    except ValueError:
-        return 1
+    pattern = r"^(0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s(AM|PM)\sto\s(0?[1-9]|1[0-2])(?::([0-5][0-9]))?\s(AM|PM)$"
+    match = re.search(pattern, s, re.IGNORECASE)
 
-    print("2:") # hora inicial
-    print(timeGiven.group(2))
-    hora_inicial = timeGiven.group(2)
+    if not match:
+        raise ValueError
 
-    print("3:") # minutos iniciais
-    print(timeGiven.group(3))
-    minutos_inicial = timeGiven.group(3)
+    start_hour, start_min, start_ampm, end_hour, end_min, end_ampm = match.groups()
 
-    print("4:") # AM/PM
-    print(timeGiven.group(4))
-    am_pm_inicial = timeGiven.group(4).upper()
+        # Hours: 9 AM to 5 PM
+        # ('9', None, 'AM', '5', None, 'PM')
 
-    print("6:") # hora final
-    print(timeGiven.group(6))
+    start_hour_24 = converte_24(start_hour, start_ampm)
+    end_hour_24 = converte_24(end_hour, end_ampm)
 
-    hora_final = timeGiven.group(6)
-    print("7:") # minutos final
-    print(timeGiven.group(7))
+    start_min = start_min if start_min else "00"
+    end_min = end_min if end_min else "00"
 
-    minutos_final = timeGiven.group(7)
-    print("8:") # AM/PM
-    print(timeGiven.group(8))
-    am_pm_final = timeGiven.group(8).upper()
+    return f'{start_hour_24}:{start_min} to {end_hour_24}:{end_min}'
 
-    if minutos_inicial == None or minutos_final == None:
-        novo_minutos = "00"
-        print(novo_minutos)
-
-    if am_pm_inicial == "AM" or am_pm_final == "AM":
-        if hora_inicial == 12:
-            hora_inicial_nova = 0
-        else:
-            hora_inicial_nova = hora_inicial
-            
-    # se for "PM"
+def converte_24(hora, ampm):
+    if ampm.upper() == "AM":
+        return 0 if hora == 12 else hora
     else:
-        if hora != 12:
-            nora_hora = hora + 12
-        else:
-            hora_nova = hora
-
-    return f'{hora_nova}:{minutos}'
-
-
-...
-# 06:00 AM to 09:30 PM
-# 6:10 AM to 9:00 PM
+        return hora if hora == 12 else int(hora) + 12
 
 if __name__ == "__main__":
     main()
